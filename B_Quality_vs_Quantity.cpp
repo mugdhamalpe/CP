8f3bec81c4c3a006bd020de3296f0e8706d434d5
mugdhamalpe <85025388+mugdhamalpe@users.noreply.github.com>
2022-03-05 23:26:56 +0530

    05/03/22

--- B_Quality_vs_Quantity.cpp
+++ B_Quality_vs_Quantity.cpp
@@ -3,37 +3,40 @@ typedef long long ll;
 
 using namespace std;
 
+void qualQuan(){
+    ll n, sum=0;
+    cin>>n;
+    ll arr[n];
+    for(ll i=0; i<n; i++){
+        cin>>arr[i];
+        sum+=arr[i];
+    }
+    ll sumR=0, sumB=0, countR=0, countB=0;
+    sort(arr, arr+n);
+    // sumR=arr[n-1];
+    // countR=1;
+    // sumB=arr[0]+arr[1];
+    // countB=2;
+    sumB=arr[0];
+    for(ll i=1; i<=n; i++){
+        sumB+=arr[i]; //sumB= from start (smallest eles)
+        if(n-i<0){
+            break;
+        }
+        sumR+=arr[n-i]; //sumR= from end (largest eles)
+        if(sumR>sumB){
+            cout<<"YES"<<endl;
+            return;
+        }
+    }
+    cout<<"NO"<<endl;
+}
+
 int main(){
     ll t;
     cin>>t;
     while(t--){
-        ll n;
-        cin>>n;
-        ll arr[n];
-        for(ll i=0; i<n; i++){
-            cin>>arr[i];
-        }
-        
-        ll sumR=0, sumB=0, countR=0, countB=0;
-        sort(arr, arr+n);
-        sumR=arr[n-1];
-        countR=1;
-        sumB=arr[0]+arr[1];
-        countB=2;
-        // for(ll i=1; i<=n; i++){
-        //     if(i%2==0){
-        //         sumR+=i;
-        //         countR+=1;
-        //     }else{
-        //         sumB+=i;
-        //         countB+=1;
-        //     }
-        // }
-        if(sumR>sumB && countR<countB){
-            cout<<"YES"<<endl;
-        }else{
-            cout<<"NO"<<endl;
-        }
+        qualQuan();
     }
     return 0;
 }
